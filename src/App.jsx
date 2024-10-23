5d41189b1ec78561994c4b069f13f39b41905db6
ROBERT-Gimenez <robertgim37@gmail.com>
2024-10-22 23:08:44 -0300

    Fix:Language change from 'ts' to 'js'

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,15 @@
-import { Route, Routes } from 'react-router-dom'
 import './App.css'
+import { Route, Routes } from "react-router-dom";
+import Home from './routes/Home';
 
 function App() {
-
   return (
-    <div className="App">
-    <Routes>
-      <Route path="*" element={<h1>Error 404 - Page not Found</h1>} />
-    </Routes>
-  </div>
+    <>
+     <Routes>
+        <Route path="/" element={<Home/>} />
+        <Route path="*" element={<h1>Error 404 - Page not Found</h1>} />
+      </Routes>
+    </>
   )
 }
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
+import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+createRoot(document.getElementById('root')).render(
   <BrowserRouter>
-    <App/>
+        <App/>
   </BrowserRouter>
 )

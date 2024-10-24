837df84b4012d38b0d95daff86bc6fa03960e4c2
ROBERT-Gimenez <robertgim37@gmail.com>
2024-10-23 21:41:51 -0300

    feat:Se agrega la url a la vista del administrador + la generacion de clases de estilos para tailwind

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import './App.css'
 import { Route, Routes } from "react-router-dom";
 import Home from './routes/Home';
+import Admin from './routes/Admin';
 
 function App() {
   return (
     <>
      <Routes>
-        <Route path="/" element={<Home/>} />
+     <Route path="/" element={<Home/>} />
+        <Route path="/admin" element={<Admin/>} />
         <Route path="*" element={<h1>Error 404 - Page not Found</h1>} />
       </Routes>
     </>
